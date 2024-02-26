ceab63bc31367c3b53d800787cb321253b9bf4c2
Nikoll Bonilla <nikoll.bonilla@tecsup.edu.pe>
2024-02-26 17:56:31 -0500

    codigo

---
+++ index.py
@@ -0,0 +1,19 @@
+# Instalar npcap y scapy (https://scapy.net)
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniif(interface):
+    scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
+
+def sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print("*", 100)
+        print(packet.show())
+        print("*", 100)
+
+def main():
+    sniif("Wi-Fi")
+
+if __name__ == "__main__":
+    main()
